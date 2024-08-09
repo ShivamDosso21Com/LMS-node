09a522c220565a367c75506ec4b4e02c2c8ecb36
vikram singh <vikramsinghkhinchi5@gmail.com>
2024-08-09 23:34:19 +0530

    added database in .env file

--- app.ts
+++ app.ts
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
+
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
--- routes/videoRoutes.ts
+++ routes/videoRoutes.ts
@@ -4,17 +4,38 @@ import { uploadVideo, getVideos } from '../controllers/videoController';
 
 const router = Router();
 
-const storage = multer.diskStorage({
+ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'upload/');
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
-const upload = multer({ storage });
+// const upload = multer({ storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB
+}).single('video'); // Adjust the size as per your needs
 
-router.post('/upload', upload.single('video'), uploadVideo);
+
+// router.post('/upload', uploadVideo);
+
+router.post('/upload', (req, res) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(413).json({ error: 'File too large' });
+      }
+    } else if (err) {
+      return res.status(500).json({ error: 'File upload failed', details: err });
+    }
+
+    // Call the uploadVideo controller if the file is uploaded successfully
+    uploadVideo(req, res);
+  });
+});
+
 router.get('/videos', getVideos);
 
 export default router;
